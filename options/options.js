c174621c577865d7db136cf432cfec0df31947c1
liqiang zhang <andy_zhanglq@139.com>
2023-02-26 22:28:00 +0800

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "ywkjmobile-39123",
   url: "https://ywkjmobile-39123.botics.co",
   api: "https://ywkjmobile-39123.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
